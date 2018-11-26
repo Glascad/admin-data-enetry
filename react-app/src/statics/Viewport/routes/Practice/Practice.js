a34c9a5f6d98ca5f5e3520d4ae970dcda2e22cac
rrackiewicz <raymondvrackiewicz@gmail.com>
2018-11-26 09:52:15 -0700

    With Practice

--- react-app/src/statics/Viewport/routes/Practice/Practice.js
+++ react-app/src/statics/Viewport/routes/Practice/Practice.js
@@ -1,3 +1,20 @@
-export default function Practice() {
-    return null;
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import { query } from './practice-gql';
+
+export default class Practice extends Component {
+    state = {};
+    render = () => {
+        return (
+            <Query 
+                query={query}
+            >
+            {() => {
+                return (
+                    <div></div>
+                )
+            }} 
+            </Query>
+        )
+    }
 }
