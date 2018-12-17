8d2121768fc553274917746c4c263bdf25a06777
Thomas Lowry <minilao94@yahoo.com>
2018-12-17 14:10:05 -0700

    fixed padding on bottom of ul

--- react-app/src/routes/System/SystemOptions/system-options-graphql.js
+++ react-app/src/routes/System/SystemOptions/system-options-graphql.js
@@ -260,6 +260,56 @@ export const mutations = {
             }
         }) => [{ query, variables: { nodeId } }]
     },
+    deleteOptionValue: {
+        mutation: gql`mutation DeleteOptionValue(
+            $nodeId:ID!
+        ){
+            deleteOptionValue(
+                input:{
+                    nodeId:$nodeId
+                }
+            ){
+                optionValue{
+                    nodeId
+                    id
+                    name
+                    value
+                    systemOptionBySystemOptionId{
+                        nodeId
+                        systemBySystemId{
+                            nodeId
+                        }
+                    }
+                }
+            }
+        }`,
+        mapResultToProps: (deletedOptionValue, { systemOptions }) => ({
+            systemOptions: systemOptions
+                .map(option => option.id === deletedOptionValue.systemOptionId ?
+                    {
+                        ...option,
+                        optionValuesBySystemOptionId: {
+                            ...option.optionValuesBySystemOptionId,
+                            nodes: option.optionValuesBySystemOptionId.nodes.filter(ov => ov.nodeId !== deletedOptionValue.nodeId)
+                        }
+                    }
+                    :
+                    option)
+        }),
+        refetchQueries: ({
+            data: {
+                deleteOptionValue: {
+                    optionValue: {
+                        systemOptionBySystemOptionId: {
+                            systemBySystemId: {
+                                nodeId
+                            }
+                        }
+                    }
+                }
+            }
+        }) => [{ query, variables: { nodeId } }]
+    },
     createSystemOptionConfigurationType: {
         mutation: gql`mutation CreateSystemOptionConfigurationType(
             $systemOptionId:Int!,
