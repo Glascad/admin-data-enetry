b3e27870e59de759bec0f686f0c760bd7d749090
Thomas Lowry <minilao94@yahoo.com>
2019-02-26 10:56:34 -0700

    working on starting bay quantity

--- src/routes/Project/Elevations/ElevationInfo/ElevationInfo.js
+++ src/routes/Project/Elevations/ElevationInfo/ElevationInfo.js
@@ -8,12 +8,24 @@ import {
 import ACTIONS from '../elevation-manager/elevation-actions';
 
 export default function ElevationInfo({
+    elevation,
     elevation: {
         name,
-        horizontalRoughOpening,
-        verticalRoughOpening,
-        elevationContainers,
-    } = {},
+        roughOpening: {
+            x,
+            y,
+        },
+        finishedFloorHeight,
+        _elevationContainers = [],
+        _containerDetails = [],
+    },
+    // elevationInput: {
+    //     name,
+    //     horizontalRoughOpening,
+    //     verticalRoughOpening,
+    //     elevationContainers = [],
+    //     finishedFloorHeight,
+    // },
     updateElevation,
 }) {
     console.log(arguments[0]);
@@ -31,19 +43,46 @@ export default function ElevationInfo({
                 })}
             />
             <Input
-                label="Horizontal Rough Opening"
+                label="Rough Opening Height"
                 type="number"
-                value={horizontalRoughOpening}
+                value={y}
                 onChange={({ target: { value } }) => updateElevation(ACTIONS.UPDATE, {
-                    horizontalRoughOpening: value,
+                    verticalRoughOpening: +value,
                 })}
             />
             <Input
-                label="Vertical Rough Opening"
+                label="Rough Opening Width"
                 type="number"
-                value={verticalRoughOpening}
+                value={x}
                 onChange={({ target: { value } }) => updateElevation(ACTIONS.UPDATE, {
-                    verticalRoughOpening: value,
+                    horizontalRoughOpening: +value,
+                })}
+            />
+            <Input
+                label="Starting Bay Quantity"
+                type="number"
+                value={_elevationContainers
+                    .filter(({ id }) => _containerDetails
+                        .some(({
+                            firstContainerId,
+                            secondContainerId,
+                            vertical,
+                        }) => !vertical
+                        &&
+                        firstContainerId === undefined
+                            &&
+                            secondContainerId === id)
+                    ).length}
+                onChange={({ target: { value } }) => updateElevation(ACTIONS.UPDATE, {
+                    startingBayQuantity: +value,
+                })}
+            />
+            <Input
+                label="Height Above Finished Floor"
+                type="number"
+                value={finishedFloorHeight}
+                onChange={({ target: { value } }) => updateElevation(ACTIONS.UPDATE, {
+                    finishedFloorHeight: +value,
                 })}
             />
         </>
