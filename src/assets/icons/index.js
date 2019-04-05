1ed3338c850daec2c1c3dfb761380116481518ef
Thomas Lowry <minilao94@yahoo.com>
2019-04-05 16:30:42 -0600

    updated some of the svgs

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,23 +1,43 @@
 import { ReactComponent as Hamburger } from './hamburger.svg';
+// ROUGH OPENING
 import { ReactComponent as StepHead } from './step-head.svg';
+// ADD
 import { ReactComponent as AddBay } from './add-bay.svg';
 import { ReactComponent as AddVertical } from './add-vertical.svg';
 import { ReactComponent as AddHorizontal } from './add-horizontal.svg';
+// EDIT
 import { ReactComponent as EditInfill } from './edit-infill.svg';
+// MERGE
+import { ReactComponent as MergeVertically } from './merge-vertically.svg';
+import { ReactComponent as MergeHorizontally } from './merge-horizontally.svg';
 import { ReactComponent as MergeUp } from './merge-up.svg';
 import { ReactComponent as MergeRight } from './merge-right.svg';
 import { ReactComponent as MergeDown } from './merge-down.svg';
 import { ReactComponent as MergeLeft } from './merge-left.svg';
+// DELETE
+import { ReactComponent as DeleteHorizontal } from './delete-horizontal.svg';
+import { ReactComponent as DeleteVertical } from './delete-vertical.svg';
+import { ReactComponent as DeleteLite } from './delete-lite.svg';
 
 export {
     Hamburger,
+    // RO
     StepHead,
+    // ADD
     AddBay,
     AddVertical,
     AddHorizontal,
+    // EDIT
     EditInfill,
+    // MERGE
+    MergeVertically,
+    MergeHorizontally,
     MergeUp,
     MergeRight,
     MergeDown,
     MergeLeft,
+    // DELETE
+    DeleteHorizontal,
+    DeleteVertical,
+    DeleteLite,
 };
