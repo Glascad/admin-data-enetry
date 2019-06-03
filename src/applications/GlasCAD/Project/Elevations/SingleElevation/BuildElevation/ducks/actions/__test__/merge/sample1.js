2e54a508ea9f59fefa20edca887b1be88839f458
AndrewGiles <giles.andrew33@gmail.com>
2019-06-03 08:43:17 -0600

    added a testing folder for Andrew

---
+++ src/applications/GlasCAD/Project/Elevations/SingleElevation/BuildElevation/ducks/actions/__test__/merge/sample1.js
@@ -0,0 +1,13 @@
+import sample1 from "../../../../../__test__/elevations/sample1.json";
+import MERGE_CONTAINERS from "../../merge-containers";
+import defaultElevationInput from "../../../default-elevation-update";
+import RecursiveElevation from "../../../../../utils/recursive-elevation/elevation"
+
+describe("sample1 merging container 710 Left", () => {
+    test("container 8 is gone", () => {
+        expect(true).toequal(true);
+    })
+});
+
+
+
