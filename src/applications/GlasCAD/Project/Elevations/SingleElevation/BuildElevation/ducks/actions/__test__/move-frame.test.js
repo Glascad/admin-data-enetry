401efc35f98b4f4bf12ecb83f0e3fdf672be3776
Thomas Lowry <thomas@lowrysoftware.com>
2019-06-04 08:48:20 -0600

    fixed move-frame.test

--- src/applications/GlasCAD/Project/Elevations/SingleElevation/BuildElevation/ducks/actions/__test__/move-frame.test.js
+++ src/applications/GlasCAD/Project/Elevations/SingleElevation/BuildElevation/ducks/actions/__test__/move-frame.test.js
@@ -19,59 +19,26 @@ const testMoveFrame = ({ name, elevation, detailId, distance, expectedDetails, d
 
     describe(`${name} frame moving a distance of ${distance}`, () => {
         test(`daylight openings are correct for all containers`, () => {
-
             daylightOpenings.forEach(({ id, ...daylightOpening }) => {
                 expect(sampleResult).toMatchObject({
                     containers: {
                         [id]: {
-                            daylightOpening
-                        }
-                    }
-                })
-
-            })
-
+                            daylightOpening,
+                        },
+                    },
+                });
+            });
         });
+
         test(`all details are correctly added or have changed their reference`, () => {
-
-
-            // expectedDetails.forEach(detail => {
-            //     expect(sampleResult.allDetails.some(({ firstContaierId, secondContainerId }) => (
-            //         (detail.firstContaierId === firstContaierId)
-            //         &&
-            //         (detail.secondContainerId === secondContainerId)
-            //     ))).toBeTruthy()
-            // });
-
-            // expectedDetails.some( detail => {
-            //     sampleResult.detailIds.map( id => {
-            //         sampleResult.details[id]
-            //     })
-            // })
-
-            // sampleResult.detailIds.map( (id) => {
-
-            // })
-
-            // expectedDetails.forEach( detail => {
-            //     expect(sampleResult.allDetails).toContainEqual(expect.objectContaining(detail));
-            // });
-
-
-
-            // expect(sampleResult.allDetails).toMatchObject(
-            //     expect.arrayContaining(
-            //         expectedDetails.map(
-            //             detail => expect.objectContaining(detail)
-            //             )
-            //     )
-            // );
-
-
-
+            expect(sampleResult.allDetails).toMatchObject(
+                expect.arrayContaining(
+                    expectedDetails.map(
+                        detail => expect.objectContaining(detail),
+                    ),
+                ),
+            );
         });
-
-
 
         test("all details are correctly deleted or have changed their reference", () => {
 
@@ -89,25 +56,21 @@ testMoveFrame({
     detailId: 1854,
     expectedDetails: [
         {
-            firstContaierId: 729,
+            firstContainerId: 729,
             secondContainerId: 738,
         },
-        // {
-        //     firstContaierId: 729,
-        //     secondContainerId: 737,
-        // },
-        // {
-        //     firstContaierId: 1,
-        //     secondContainerId: 2,
-        // },
+        {
+            firstContainerId: 729,
+            secondContainerId: 737,
+        },
     ],
     deletedDetails: [
         {
-            firstContaierId: 732,
+            firstContainerId: 732,
             secondContainerId: 736,
         },
         {
-            firstContaierId: 732,
+            firstContainerId: 732,
             secondContainerId: 737,
         },
     ],
@@ -115,12 +78,12 @@ testMoveFrame({
         {
             id: 732,
             x: 185,
-            y: 50
+            y: 50,
         },
         {
             id: 729,
             x: 185,
-            y: 370
+            y: 370,
         },
     ],
 });
