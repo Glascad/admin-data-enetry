1a648473ec78814f37180c50cc743826c87f48ee
Thomas Lowry <thomas@lowrysoftware.com>
2019-06-25 13:41:43 -0600

    wrote test dimension function

--- src/Applications/GlasCAD/Project/Elevations/SingleElevation/utils/recursive-elevation/__test__/dimension.test.js
+++ src/Applications/GlasCAD/Project/Elevations/SingleElevation/utils/recursive-elevation/__test__/dimension.test.js
@@ -1,33 +1,61 @@
 
 import RecursiveElevation from "../elevation";
 
+import '../../../../../../../../../public';
+
 import sample6 from '../../../__test__/sample-elevations/sample6.json';
 
-function testDimensions({ elevation, vertical = [], horizontal = [] }) {
+function testDimensions({
+    elevation,
+    verticalTrackCount,
+    vertical = [],
+    horizontalTrackCount,
+    horizontal = [],
+}) {
     describe(`Testing dimensions of elevation ${elevation.name}`, () => {
         const recursiveElevation = new RecursiveElevation(elevation);
-        vertical.forEach((track, i) => {
-            const actualTrack = recursiveElevation.verticalContainerDimensionTracks[i];
+
+        test(`Elevation has correct number of vertical dimension tracks`, () => {
+            expect(recursiveElevation.verticalContainerDimensionTracks).toHaveProperty('length', verticalTrackCount);
+        });
+
+        test(`Elevation has correct number of horizontal dimension tracks`, () => {
+            expect(recursiveElevation.horizontalContainerDimensionTracks).toHaveProperty('length', horizontalTrackCount);
+        });
+
+        function testTrack(vertical, track, i) {
+            const actualTrack = recursiveElevation[`${vertical ? 'vertical' : 'horizontal'}ContainerDimensionTracks`][i];
             track.forEach(({ containerIds, dimension, offset }) => {
-                containerIds.forEach(id => {
-                    test(`Vertical container dimension track #${i} contains a dimension with container id ${id}`, () => {
-                        expect(actualTrack).toEqual(
-                            // array of dimensions
-                            expect.arrayContaining([
-                                // dimension object
-                                expect.objectContaining({
-                                    // container array
-                                    containers: expect.arrayContaining([
+                test(`${vertical ? 'Vertical' : 'Horizontal'} container dimension track #${i} contains a dimension with all container ids ${containerIds.join(', ')}, dimension ${dimension}, and offset ${offset}`, () => {
+                    expect(actualTrack).toEqual(
+                        // array of dimensions
+                        expect.arrayContaining([
+                            // dimension object
+                            expect.objectContaining({
+                                // container array
+                                containers: expect.arrayContaining(
+                                    // expected container ids
+                                    containerIds.map(id => (
                                         // actual container
                                         expect.objectContaining({ id })
-                                    ])
-                                })
-                            ])
-                        )
-                    });
+                                    ))
+                                ),
+                                dimension,
+                                offset,
+                            }),
+                        ]),
+                    );
                 });
             });
-        })
+        }
+
+        vertical.forEach((track, i) => {
+            testTrack(true, track, i);
+        });
+
+        horizontal.forEach((track, i) => {
+            testTrack(false, track, i);
+        });
     });
 }
 
@@ -38,7 +66,7 @@ testDimensions({
         // track #1 -- not sorted - no order
         [
             {
-                // containerIds: [],
+                containerIds: [],
                 // dimension: 120,
                 // offset: 120,
             },
