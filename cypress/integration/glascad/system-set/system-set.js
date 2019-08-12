efaf4dd23904e5f3c068be9ce236381ae8c1e49b
Thomas Lowry <thomas@lowrysoftware.com>
2019-08-12 14:06:03 -0600

    added systemSetDetailTypeConfigurationTypes to SystemSet

--- cypress/integration/glascad/system-set/system-set.js
+++ cypress/integration/glascad/system-set/system-set.js
@@ -2,9 +2,13 @@
 describe('System Set Tests', () => {
     beforeEach(() => {
         cy.login();
-        cy.visit('http://localhost:3000/glascad/project/sets/system-set/info?projectId=1')
+        cy.visit('http://localhost:3000/glascad/project/sets/system-set/info?projectId=2&systemSetId=2');
     });
-    it('Can visit the page', () => {
+    // it('Can visit the page', () => {
         
+    // });
+    it('Can update manufacturer and system', () => {
+        cy.get('[data-cy="manufacturer"]').type('kawneer');
+        cy.get('[data-cy="system-id"]').type('trifab451');
     });
 });
