a2d73122d793583d025544aa4c69816798c4adeb
AndrewGiles <giles.andrew33@gmail.com>
2019-08-26 09:30:23 -0600

    fixed a little more

--- src/components/state/TransformContext.js
+++ src/components/state/TransformContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useRef } from 'react';
 import { withRouter } from 'react-router-dom';
-import { withContext, useInitialState } from '../../components';
+import useInitialState from '../hooks/use-initial-state';
+import withContext from '../higher-order/with-context';
 
 export const TransformContext = createContext();
 
