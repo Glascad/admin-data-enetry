cb5b0c9b190a3042d76fea9342dfea13deac40d6
rrackiewicz <raymondvrackiewicz@gmail.com>
2019-08-29 13:52:34 -0600

    Started FindChildren util

--- src/Applications/DataEntry/System/SystemBuilder/ducks/utils.js
+++ src/Applications/DataEntry/System/SystemBuilder/ducks/utils.js
@@ -1,21 +1,23 @@
+import { match } from '../../../../../utils';
 
 export const getFirstItem = ({ _systemOptions }) => _systemOptions.find(({ parentSystemOptionValueId }) => !parentSystemOptionValueId);
 
-export const getChildren = (item, system) => {
-    const {
-        __typename,
-        id,
-    } = item;
-
-    // if (__typename === "SystemOption") {
-
-    // } else if (__typename === "SystemOptionValue") {
-
-    // } else if (__typename === "DetailOptionValue")
-};
-
+export const getChildren = ({
+    __typename,
+    id,
+    _systemOptionValues,
+    _detailOptionValues,
+    _configurationOptionValues,
+}, system) => match(__typename)
+    .equals('SystemOption', () => _systemOptionValues)
+    .equals('SystemOptionValue', () => { })
+    .equals('SystemDetailType', () => { })
+    .equals('DetailOption', () => _detailOptionValues)
+    .equals('DetailOptionValue', () => { })
+    .equals('SystemConfigurationType', () => { })
+    .equals('ConfigurationOption', () => _configurationOptionValues)
+    .equals('ConfigurationOptionValue', () => { })
+    .finally(() => { });
 export const makeRenderable = ({
 
-}) => {
-
-};
+}) => { }
--- src/utils/functions/match.js
+++ src/utils/functions/match.js
@@ -6,12 +6,14 @@ const final = x => ({
 const matched = x => ({
     on: () => matched(x),
     condition: () => matched(x),
+    equals: () => matched(x),
     otherwise: () => final(x),
 });
 
 const match = x => ({
     on: (pred, fn) => (pred(x) ? matched(fn(x)) : match(x)),
     condition: (condition, fn) => (condition ? matched(fn(x)) : match(x)),
+    equals: (val, fn) => (x === val ? matched(fn(x)) : match(x)) ,
     otherwise: fn => final(fn(x)),
 });
 
