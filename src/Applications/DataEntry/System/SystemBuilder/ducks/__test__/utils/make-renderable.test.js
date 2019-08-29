0030f007c31b0caaf218aec5f3ccf81644e3a1fe
Thomas Lowry <thomas@lowrysoftware.com>
2019-08-29 13:17:43 -0600

    added some functionality to the match function

--- src/Applications/DataEntry/System/SystemBuilder/ducks/__test__/utils/make-renderable.test.js
+++ src/Applications/DataEntry/System/SystemBuilder/ducks/__test__/utils/make-renderable.test.js
@@ -8,26 +8,29 @@ function testMakeRenderable(system) {
         test(`all nodes have correct shape`, () => {
             const nodesHaveCorrectShape = ({
                 item: {
-                    __typename,
+                    __typename = '',
                 } = {},
-                branches,
+                branches = [],
             } = {},
-                parentTypeName = '') => {
-                match(parentTypeName)
-                    .condition(parentTypeName.match(/option/i), () => {
-                        expect(__typename).toMatch(/value$/i);
-                    })
-                    .condition(parentTypeName.match(/value/i), () => {
-                        expect(__typename).toMatch(/(type|value)$/i);
-                    })
-                    .condition(parentTypeName === '' || parentTypeName.match(/type/), () => {
-                        expect(__typename).toMatch(/option$/i);
-                    })
-                    .otherwise(() => {
-                        throw new Error(`Expected option value or type __typename, received ${__typename}`);
-                    });
-                branches.forEach(branch => nodesHaveCorrectShape(branch, __typename));
-            };
+                parentTypeName = '',
+            ) => (
+                    match(parentTypeName)
+                        .condition(parentTypeName.match(/option/i), tn => {
+                            expect(tn).toMatch(/value$/i);
+                        })
+                        .condition(parentTypeName.match(/value/i), tn => {
+                            expect(tn).toMatch(/(type|value)$/i);
+                        })
+                        .condition(parentTypeName === '' || parentTypeName.match(/type/), tn => {
+                            expect(tn).toMatch(/option$/i);
+                        })
+                        .otherwise(tn => {
+                            throw new Error(`Expected option value or type __typename, received ${tn}`);
+                        })
+                        .finally(() => {
+                            branches.forEach(branch => nodesHaveCorrectShape(branch, __typename));
+                        })
+                );
             nodesHaveCorrectShape(result);
         });
     });
--- src/utils/functions/match.js
+++ src/utils/functions/match.js
@@ -1,14 +1,18 @@
 
+const final = x => ({
+    finally: cb => cb(x),
+});
+
 const matched = x => ({
     on: () => matched(x),
     condition: () => matched(x),
-    otherwise: () => x,
+    otherwise: () => final(x),
 });
 
 const match = x => ({
     on: (pred, fn) => (pred(x) ? matched(fn(x)) : match(x)),
     condition: (condition, fn) => (condition ? matched(fn(x)) : match(x)),
-    otherwise: fn => fn(x),
+    otherwise: fn => final(fn(x)),
 });
 
 export default match;
