c97b6a1cb1830381eb061c9e1b949220377a3f40
Thomas Lowry <thomas@lowrysoftware.com>
2019-08-29 13:52:56 -0600

    added regex to match

--- src/utils/functions/match.js
+++ src/utils/functions/match.js
@@ -6,12 +6,20 @@ const final = x => ({
 const matched = x => ({
     on: () => matched(x),
     condition: () => matched(x),
+    regex: typeof x === 'string' ?
+        () => matched(x)
+        :
+        () => { throw new Error(`Cannot use \`regex()\` on non-string match`) },
     otherwise: () => final(x),
 });
 
 const match = x => ({
-    on: (pred, fn) => (pred(x) ? matched(fn(x)) : match(x)),
-    condition: (condition, fn) => (condition ? matched(fn(x)) : match(x)),
+    on: (pred, fn) => pred(x) ? matched(fn(x)) : match(x),
+    condition: (condition, fn) => condition ? matched(fn(x)) : match(x),
+    regex: typeof x === 'string' ?
+        (regex, fn) => x.match(regex) ? matched(fn(x)) : match(x)
+        :
+        () => { throw new Error(`Cannot use \`regex()\` on non-string match`) },
     otherwise: fn => final(fn(x)),
 });
 
