ed0cedff6cf582ca34e6efc2b1f56e7c18c752c2
rrackiewicz <raymondvrackiewicz@gmail.com>
2019-08-29 14:43:00 -0600

    Start of GetChildren util

--- src/Applications/DataEntry/System/SystemBuilder/ducks/utils.js
+++ src/Applications/DataEntry/System/SystemBuilder/ducks/utils.js
@@ -6,15 +6,22 @@ export const getChildren = ({
     __typename,
     id,
     _systemOptionValues,
+    _systemDetailTypes, 
     _detailOptionValues,
+    _systemConfigurationTypes,
     _configurationOptionValues,
-}, system) => match(__typename)
+}, {
+    _systemOptions,
+    _detailOptions,
+    _configurationOptions,
+}) => match(__typename)
     .equals('SystemOption', () => _systemOptionValues)
-    .equals('SystemOptionValue', () => { })
-    .equals('SystemDetailType', () => { })
+    .equals('SystemOptionValue', () => _systemOptions.filter(({ parentSystemOptionValueId }) => parentSystemOptionValueId === id)
+        .concat(_systemDetailTypes))    
+    .equals('SystemDetailType', () => _detailOptions.filter(({ systemDetailTypeId }) => systemDetailTypeId === id))
     .equals('DetailOption', () => _detailOptionValues)
     .equals('DetailOptionValue', () => { })
-    .equals('SystemConfigurationType', () => { })
+    .equals('SystemConfigurationType', () => _configurationOptions.filter(({ systemConfigurationTypeId }) => systemConfigurationTypeId === id))
     .equals('ConfigurationOption', () => _configurationOptionValues)
     .equals('ConfigurationOptionValue', () => { })
     .finally(() => { });
