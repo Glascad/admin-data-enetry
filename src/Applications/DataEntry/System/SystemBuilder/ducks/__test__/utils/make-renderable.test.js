7bea37ccd763fa6a6d5009a8c6bbc2ff2a0fa4d8
rrackiewicz <raymondvrackiewicz@gmail.com>
2019-08-29 12:26:17 -0600

    Added unit test MakeRenderable

--- src/Applications/DataEntry/System/SystemBuilder/ducks/__test__/utils/make-renderable.test.js
+++ src/Applications/DataEntry/System/SystemBuilder/ducks/__test__/utils/make-renderable.test.js
@@ -1,3 +1,41 @@
-import { makeRenderable } from "../../utils";
+import { makeRenderable } from '../../utils';
+import { system1 } from '../../../../sample-systems';
 
+function testMakeRenderable(system) {
+    describe(`Testing MakeRenderable on ${system.name}`, () => {
+        const result = makeRenderable(system);
+        test(`all nodes have correct shape`, () => {
+            const nodesHaveCorrectShape = ({
+                item: {
+                    __typename
+                },
+                branches
+            }, parentTypeName = '') => {
+                switch (parentTypeName
+                    .replace(/^.*(option|value|type)$/i, '$1')
+                    .toLowerCase()) {
+                    case 'option':
+                        expect(__typename).toMatch(/value$/i);
+                        break;
+                    case 'value':
+                        expect(__typename).toMatch(/(type|value)$/i);
+                        break;
+                    case 'type':
+                    case '':
+                        expect(__typename).toMatch(/option$/i);
+                        break;
+                    default:
+                        throw new Error(`Expected option value or type __typename, received ${__typename}`);
+                }
+                // expect(node).toMatchObject({
+                //     item: expect.objectContaining({}),
+                //     branches: expect.arrayContaining([]),
+                // });
+                branches.forEach(branch => nodesHaveCorrectShape(branch, __typename));
+            };
+            nodesHaveCorrectShape(result);
+        });
+    });
+}
 
+testMakeRenderable(system1)
