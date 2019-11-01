39bfcd676a4bb26ba60fa074eadbcc20e8ea8670
Thomas Lowry <thomas@lowrysoftware.com>
2019-11-01 14:51:52 -0600

    reorganized getting color codes

--- src/utils/functions/dxf-to-svg.js
+++ src/utils/functions/dxf-to-svg.js
@@ -12,27 +12,7 @@ import {
     rotatePoint,
     transformPoint,
 } from './trig';
-import COLOR_CODES from './color-index.json';
-
-const getColorFromColorCode = colorCode => {
-    const colorObj = COLOR_CODES[colorCode];
-    if (colorObj) {
-        const { R, G, B } = colorObj;
-
-        const numberToString = num => {
-            if (typeof num !== 'number') return num;
-            else {
-                const str = num.toFixed(0);
-                return str.length === 1 ?
-                    `0${str}`
-                    :
-                    str;
-            }
-        }
-
-        return `#${[R, G, B].map(numberToString).join('')}`;
-    }
-}
+import getColorFromColorCode from './get-color-from-color-code';
 
 export default dxf => {
 
