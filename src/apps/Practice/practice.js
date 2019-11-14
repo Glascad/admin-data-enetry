f49163106759258bd86ea74b7b3d18f5dc3b269a
AndrewGiles <giles.andrew33@gmail.com>
2019-11-14 15:55:47 -0700

    updated git ignore

--- src/apps/Practice/practice.js
+++ src/apps/Practice/practice.js
@@ -1,54 +1,76 @@
+import React, { useState } from 'react';
+import Statics from '../Statics/Statics';
+import { TitleBar, Pill, ListWrapper, Tree, useQuery, RightSidebar, Input, GroupingBox, Toggle } from '../../components';
 
-// (function () {
+import './Practice.scss';
 
-//     function update(arr) {
-//         return arr.reduce(([updated, deleted], item) => {
-//             if (shouldDelete(item)) return [updated, deleted.concat(item)];
-//             else return [updated.concat(item), deleted];
-//         }, [[], []]);
-//     }
+function Practice() {
+    return (
+        <div id="Practice">
+            <TitleBar
+                title="Test Options"
+            />
+            <RightSidebar
+                open={true}
+                View={{
+                    title: "Sidebar",
+                    component: () => (
+                        <>
+                            <TitleBar
+                                title="Configurations"
+                            />
+                            {["Head", "Shim Support", "Compensating Receptor", "Infills"].map(ct => (
+                                <Input
+                                    label={ct}
+                                    type="switch"
+                                    checked={true}
+                                />
+                            ))}
+                            {/* <GroupingBox
+                                title="Compensating Receptor"
+                            // switch={{}}
+                            > */}
+                            {/* <Toggle
+                                    label="Durability"
+                                    buttons={[
+                                        {
+                                            text: "Standard Duty",
+                                        },
+                                        {
+                                            text: "High-Performance",
+                                            selected: true,
+                                        },
+                                    ]}
+                                    />
+                                <Toggle
+                                    label="Other Option"
+                                    buttons={[
+                                        {
+                                            text: "Value 1",
+                                            selected: true,
+                                        },
+                                        {
+                                            text: "Value 2",
+                                        },
+                                    ]}
+                                /> */}
+                            {/* {["Standard Duty", "High-Performance"].map(ov => (
+                                    <Input
+                                        label={ov}
+                                        type="switch"
+                                    />
+                                ))} */}
+                            {/* </GroupingBox> */}
+                            <TitleBar
+                                title="Infills"
+                            />
+                        </>
+                    )
+                }}
+            />
+        </div>
+    );
+}
 
-//     const [sos, sosToDelete] = update(sos);
-//     const [sovs, sovsToDelete] = update(sovs);
-//     const [sds, sdsToDelete] = update(sds);
-//     const [dos, dosToDelete] = update(dos);
-//     const [dovs, dovsToDelete] = update(dovs);
-//     const [scs, scsToDelete] = update(scs);
-//     const [cos, cosToDelete] = update(cos);
-//     const [covs, covsToDelete] = update(covs);
 
-//     return {
-//         deletions: [
-//             ...sosToDelete,
-//             ...sovsToDelete,
-//             ...sdsToDelete,
-//             ...dosToDelete,
-//             ...dovsToDelete,
-//             ...scsToDelete,
-//             ...cosToDelete,
-//             ...covsToDelete,
-//         ],
-//         sos,
-//         sovs,
-//         sds,
-//         dos,
-//         dovs,
-//         scs,
-//         cos,
-//         covs,
-//     };
-// });
-
-
-
-// MOVING AN ITEM
-// not selectable becomes disabled
-// cahnge the onclick to do whatever needs to be done
-
-// Options = type, values -> end node and not already in path.
-//     Values = Option -> options with the same name, not already included.
-//     Types - > Values
-//         Detail -> terminal system Option value -> details or none and not current detail
-//         Configuration -> same thing, but detail Values.
-
-// Voids will make a difference
+export default () => <Statics routes={{ Practice }} />
